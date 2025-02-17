7f4ce6bfac00b52dfc5d49a55ac6368a3e045e8f
rajkumar <rajkumars@melss.com>
2025-02-17 08:23:13 +0530

    data_cleaning

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -2,9 +2,22 @@ import pandas as pd
 import numpy as np
 from zenml import step
 import logging
+from src.data_cleaning import DataCleaning,DataDivideStrategy,DataPreprocessStrategy
 
 
 
 @step
 def clean_df(df):
-    pass
+    try:
+        process_strategy = DataPreprocessStrategy()
+        data_cleaning = DataCleaning(df,process_strategy)
+        processed_data = data_cleaning.handle_data()
+        divide_strategy = DataDivideStrategy()
+        data_cleaning = DataCleaning(processed_data,divide_strategy)
+        X_train,X_test,y_train,y_test = data_cleaning.handle_data()
+        logging.info("data cleaning completed")
+        return X_train,X_test,y_train,y_test
+        
+    except Exception as e:
+        logging.error(f"Error while cleaning data:{e}")
+        raise e
